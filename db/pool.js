a4e3bada1d954ec7ba585da0a8fdc461d973f59d
Vishnu-Krishnakumar <vkrish3@wgu.edu>
2025-03-14 20:36:22 +0530

    test

--- db/pool.js
+++ db/pool.js
@@ -4,6 +4,6 @@ module.exports = new Pool({
     host: process.env.host, // or wherever the db is hosted
     user: process.env.user,
     database: process.env.database,
-    password: process.env.password,
+    password: "lbfrtQxihZexXZkcyPrpYEdlDUjwdCyQ",
     port: 5432 // The default port
   });
